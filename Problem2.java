008348a598c9a6a6bbc41ad6d4b5e315a9206ad2
Brennan <bmhuber42@students.tntech.edu>
2016-11-28 14:45:09 -0500

    Verify convergence then done.

--- Problem2.java
+++ Problem2.java
@@ -1,19 +1,80 @@
 // Brennan Huber
 // MATH 4600
 
-// uh yeah no idea where to begin.. gonna have to look at the notes for this one.
-
 public class Problem2 {
-	/*
-	 * 		Write a program to apply the modified newtons method to the equation f(x) = x^3 - 3x^2 + 4 = 0
-	 * 		starting with x0 = 3. use m=1 and 2, and make separate numerical runs. In each case, set the maximum number of iterations to 25,
-	 * 		but stop the computation when the backward error, i.e f(xn), is less than 10^-12. Print all intermediate points xn and backwards
-	 * 		error f(xn). Verify the convergence rates of your numerical solutions in both cases. (For m=1, you should observe linear convergence.
-	 * 		For m=2, you should get quadratic convergence.) Use double precision in your computation. 
-	 */
+	
+	// all thats left to do is verify convergence.
+	// m=1 should be linear convergence.
+	// m=2 should be quadratic. 
 	
 	public static void main(String args[]) {
+
+		// Initializing variables.
+		int N = 25;
 		
+		double x_m1[] = new double[N];
+		double x_m2[] = new double[N];
+		x_m1[0] = 3.0;
+		x_m2[0] = 3.0;
+		
+		int m1 = 1;
+		int m2 = 2;
+		
+		double error = Math.pow(10, -12);
+		double backwards_error = 0.0;
+		
+		// backwards error of approximation x_c is abs(f(x_c))
+		
+		for(int i = 0; i < N - 1; i++) {
+			
+			// calculating our approximation.
+			x_m1[i + 1] = x_m1[i] - m1*(f(x_m1[i])/fPrime(x_m1[i]));
+			
+			// calculating backwards error.
+			backwards_error = Math.abs(f(x_m1[i]));
+			
+			// printing intermediate points, and backwards error.
+			System.out.println("x_" + i + " = " + x_m1[i]);
+			System.out.println("Backwards error = " + backwards_error);
+			
+			// if the backwards error is lower than we want, we are exiting the loop.
+			if(backwards_error < error) {
+				System.out.println("Backwards error is now lower than designated error: Stopping computation.");
+				break;
+			}
+		}
+		
+		System.out.println();
+		
+		for(int i = 0; i < N - 1; i++) {
+			
+			// calculating approximation
+			x_m2[i + 1] = x_m2[i] - m2*(f(x_m2[i])/fPrime(x_m2[i]));
+			
+			// calculating backwards error
+			backwards_error = Math.abs(f(x_m1[i]));
+
+			// printing intermediate points, and backwards error.
+			System.out.println("x_" + i + " = " + x_m2[i]);
+			System.out.println("Backwards error = " + backwards_error);
+
+			// if the backwards error is lower than we want, we are exiting the loop.
+			if(backwards_error < error) {
+				System.out.println("Backwards error is now lower than designated error: Stopping computation.");
+				break;
+			}
+		}
+		
+	}
+	
+	// given formula: f(x)
+	private static double f(double z) {
+		return (z*z*z) - 3*(z*z) + 4;
+	}
+	
+	// derivative of given formula: f'(x)
+	private static double fPrime(double z) {
+		return 3*(z*z) - 6*z;
 	}
 
 }
